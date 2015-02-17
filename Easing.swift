fc6a15ed5ee89386fb14210e96acba6fcb94aeb9
John Grange <john.grange@ymedialabs.com>
2015-02-17 13:30:10 -0800

    - Add initial port of Easing functions
    Only quadratic functions are included at this time
    
    Signed-off-by: John Grange <john.grange@ymedialabs.com>

---
+++ Easing.swift
@@ -0,0 +1,68 @@
+//
+//  Easing.swift
+//  SpriteKitEasingSwift
+//
+//  Created by John Grange on 2/17/15.
+//  Copyright (c) 2015 SD Networks. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+/**
+
+Modeled after the line y = x
+
+:param: perecentTimeElapsed Double repreesenting a the time elasped as value from 0.0 -> 1.0
+
+:returns: Double modfied with the Liniear function y = x
+*/
+func LinearInterpolation(perecentTimeElapsed: Double) -> Double {
+    
+    return perecentTimeElapsed
+    
+}
+
+/**
+
+Modeled after the parabola y = x^2
+
+:param: perecentTimeElapsed Double repreesenting a the time elasped as value from 0.0 -> 1.0
+
+:returns: Double modfied with the parabola function y = x^2
+*/
+func QuadraticEaseIn(perecentTimeElapsed: Double) -> Double {
+    
+    return perecentTimeElapsed * perecentTimeElapsed
+}
+
+/**
+
+Modeled after the piecewise quadratic
+    y = (1/2)((2x)^2)             ; [0, 0.5)
+    y = -(1/2)((2x-1)*(2x-3) - 1) ; [0.5, 1]
+
+:param: perecentTimeElapsed Double repreesenting a the time elasped as value from 0.0 -> 1.0
+
+:returns: Double modfied with the piecewise function :
+    y = (1/2)((2x)^2)             ; [0, 0.5)
+    y = -(1/2)((2x-1)*(2x-3) - 1) ; [0.5, 1]
+
+*/
+func QuadraticEaseOut(percentTimeElapsed: Double) -> Double {
+    
+    return -(percentTimeElapsed * (percentTimeElapsed - 2))
+}
+
+func QuadraticEaseInOut(percentTimeElapsed: Double) -> Double {
+    
+    if(percentTimeElapsed < 0.5) {
+        
+        return 2 * percentTimeElapsed * percentTimeElapsed
+    }
+    else {
+        
+        return (-2 * percentTimeElapsed * percentTimeElapsed) + (4 * percentTimeElapsed) - 1
+    }
+}
---
+++ SKAction+Easing.swift
@@ -0,0 +1,17 @@
+//
+//  SKAction+Easing.swift
+//  SpriteKitEasingSwift
+//
+//  Created by John Grange on 2/17/15.
+//  Copyright (c) 2015 SD Networks. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+extension SKAction {
+    
+    
+    
+    
+}
