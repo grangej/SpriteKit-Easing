a30c9dc6c094729c74c5182e88ed7a1d23e52516
John Grange <john.grange@ymedialabs.com>
2015-02-17 12:20:49 -0800

    Convert to swift dynamic library
    Signed-off-by: John Grange <john.grange@ymedialabs.com>

---
+++ SpriteKit-Easing-Swift/SpriteKit-Easing-Swift.h
@@ -0,0 +1,19 @@
+//
+//  SpriteKit-Easing-Swift.h
+//  SpriteKit-Easing-Swift
+//
+//  Created by John Grange on 2/17/15.
+//  Copyright (c) 2015 SD Networks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SpriteKit-Easing-Swift.
+FOUNDATION_EXPORT double SpriteKit_Easing_SwiftVersionNumber;
+
+//! Project version string for SpriteKit-Easing-Swift.
+FOUNDATION_EXPORT const unsigned char SpriteKit_Easing_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpriteKit_Easing_Swift/PublicHeader.h>
+
+
