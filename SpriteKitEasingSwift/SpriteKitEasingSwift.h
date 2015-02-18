a60ea373907ae77dcad021205e05674bd8d8296b
John Grange <john.grange@ymedialabs.com>
2015-02-17 20:36:35 -0800

    - Move / Rename addtional files dynamic libraries don't like dashes
    Signed-off-by: John Grange <john.grange@ymedialabs.com>

--- SpriteKitEasingSwift/SpriteKitEasingSwift.h
+++ SpriteKitEasingSwift/SpriteKitEasingSwift.h
@@ -1,6 +1,6 @@
 //
-//  SpriteKit-Easing-Swift.h
-//  SpriteKit-Easing-Swift
+//  SpriteKitEasingSwift.h
+//  SpriteKitEasingSwift
 //
 //  Created by John Grange on 2/17/15.
 //  Copyright (c) 2015 SD Networks. All rights reserved.
