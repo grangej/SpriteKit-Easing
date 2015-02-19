1a81c12149a8400108d4e831a017b5f600845da9
John Grange <john.grange@ymedialabs.com>
2015-02-19 15:01:27 -0800

    - Complete new conveince functions for moving, scaling, rotating, and fading
    Signed-off-by: John Grange <john.grange@ymedialabs.com>

--- SpriteKitEasingSwift/SKAction+Easing.swift
+++ SpriteKitEasingSwift/SKAction+Easing.swift
@@ -51,6 +51,24 @@ extension SKAction {
             currentFunction = linearInterpolation
         case .CurveTypeQuadratic:
             currentFunction = (mode == EasingMode.EaseIn) ? quadraticEaseIn : (mode == EasingMode.EaseOut) ? quadraticEaseOut : quadraticEaseInOut
+        case .CurveTypeCubic:
+            currentFunction = (mode == EasingMode.EaseIn) ? cubicEaseIn : (mode == EasingMode.EaseOut) ? cubicEaseOut : cubicEaseInOut
+        case .CurveTypeQuartic:
+            currentFunction = (mode == EasingMode.EaseIn) ? quarticEaseIn : (mode == EasingMode.EaseOut) ? quarticEaseOut : quarticEaseInOut
+        case .CurveTypeQuintic:
+            currentFunction = (mode == EasingMode.EaseIn) ? quinticEaseIn : (mode == EasingMode.EaseOut) ? quinticEaseOut : quinticEaseInOut
+        case .CurveTypeSine:
+            currentFunction = (mode == EasingMode.EaseIn) ? sineEaseIn : (mode == EasingMode.EaseOut) ? sineEaseOut : sineEaseInOut
+        case .CurveTypeCircular:
+            currentFunction = (mode == EasingMode.EaseIn) ? circularEaseIn : (mode == EasingMode.EaseOut) ? circularEaseOut : circularEaseInOut
+        case .CurveTypeExpo:
+            currentFunction = (mode == EasingMode.EaseIn) ? exponentialEaseIn : (mode == EasingMode.EaseOut) ? exponentialEaseOut : exponentialEaseInOut
+        case .CurveTypeElastic:
+            currentFunction = (mode == EasingMode.EaseIn) ? elasticEaseIn : (mode == EasingMode.EaseOut) ? elasticEaseOut : elasticEaseInOut
+        case .CurveTypeBack:
+            currentFunction = (mode == EasingMode.EaseIn) ? backEaseIn : (mode == EasingMode.EaseOut) ? backEaseOut : backEaseInOut
+        case .CurveTypeBounce:
+            currentFunction = (mode == EasingMode.EaseIn) ? bounceEaseIn : (mode == EasingMode.EaseOut) ? bounceEaseOut : bounceEaseInOut
         default:
             break
         }
@@ -120,6 +138,7 @@ extension SKAction {
     Move the node from the current position to a new postion using the selected curve and easing mode
     
     :param: targetNode SKNode to move
+    :param: fromVector  optional CGVector representing the start position, otherwise it will use the nodes current position
     :param: toVector   CGVector representing the end position
     :param: curveType  CurveType
     :param: mode       EasingMode
@@ -127,15 +146,25 @@ extension SKAction {
     
     :returns: SKAction
     */
-    class func moveNode(targetNode: SKNode, toVector: CGVector, curveType: CurveType, mode: EasingMode, duration: NSTimeInterval) -> SKAction? {
+    class func moveNode(targetNode: SKNode, fromVector: CGVector?, toVector: CGVector, curveType: CurveType, mode: EasingMode, duration: NSTimeInterval) -> SKAction? {
         
-        let startVector = CGVectorMake(targetNode.position.x, targetNode.position.y)
+        var startPosition: CGVector
+        
+        if let fromVector = fromVector {
+            
+            startPosition = fromVector
+        }
+        else {
+            
+            startPosition = CGVectorMake(targetNode.position.x, targetNode.position.y)
+        }
+        
         
         var action : SKAction?
         
         if let easingFunction = SKAction.getEaseFunctionForCurveType(curveType, mode: mode) {
             
-            action = self.createPointTween(startVector, endPosition: toVector, duration: duration, easingFunction: easingFunction) { (node, point) -> () in
+            action = self.createPointTween(startPosition, endPosition: toVector, duration: duration, easingFunction: easingFunction) { (node, point) -> () in
                 
                 node.position = point
             }
@@ -144,5 +173,127 @@ extension SKAction {
         return action
 
     }
+    
+    /**
+    Scale the node from the current scale to a new scale value using the selected curve and easing mode
+    
+    :param: targetNode SKNode to scale
+    :param: fromValue  optional CGFloat representing the start scale, otherwise it will use the nodes current scale
+    :param: toValue    CGFloat representing the end scale value
+    :param: curveType  CurveType
+    :param: mode       EasingMode
+    :param: duration   NSTimerInterval representing the duration of the animation
+    
+    :returns: SKAction
+    */
+    class func scaleNode(targetNode: SKNode, fromValue: CGFloat?, toValue: CGFloat, curveType: CurveType, mode: EasingMode, duration: NSTimeInterval) -> SKAction? {
+        
+        
+        var startValue: CGFloat
+        
+        if let fromValue = fromValue {
+            
+            startValue = fromValue
+        }
+        else {
+            
+            startValue = targetNode.xScale
+        }
+        
+        var action : SKAction?
+        
+        if let easingFunction = SKAction.getEaseFunctionForCurveType(curveType, mode: mode) {
+            
+            action = SKAction.createFloatTween(startValue, endFloat: toValue, duration: duration, easingFunction: easingFunction, setterBlock: { (node, float) -> () in
+                
+                node.setScale(float)
+            })
+        }
+        
+        return action
+        
+    }
+
+    /**
+    Rotate the node to the new rotation value using the selected curve and easing mode
+    
+    :param: targetNode SKNode to rotate
+    :param: fromValue  optional CGFloat representing the start rotation, otherwise it will use the nodes current zRotation
+    :param: toValue    CGFloat representing the end scale value
+    :param: curveType  CurveType
+    :param: mode       EasingMode
+    :param: duration   NSTimerInterval representing the duration of the animation
+    
+    :returns: SKAction
+    */
+    class func rotateNode(targetNode: SKNode, fromValue: CGFloat?, toValue: CGFloat, curveType: CurveType, mode: EasingMode, duration: NSTimeInterval) -> SKAction? {
+        
+        
+        var startValue: CGFloat
+        
+        if let fromValue = fromValue {
+            
+            startValue = fromValue
+        }
+        else {
+            
+            startValue = targetNode.zRotation
+        }
+        
+        var action : SKAction?
+        
+        if let easingFunction = SKAction.getEaseFunctionForCurveType(curveType, mode: mode) {
+            
+            action = SKAction.createFloatTween(startValue, endFloat: toValue, duration: duration, easingFunction: easingFunction, setterBlock: { (node, float) -> () in
+                
+                node.zRotation = float
+            })
+        }
+        
+        return action
+        
+    }
+    
+    /**
+    Fade the node to the new alpha value using the selected curve and easing mode
+    
+    :param: targetNode SKNode to fade
+    :param: fromValue  optional CGFloat representing the starting alpha value, otherwise it will use the nodes current alpha value
+    :param: toValue    CGFloat representing the ending alpha value
+    :param: curveType  CurveType
+    :param: mode       EasingMode
+    :param: duration   NSTimerInterval representing the duration of the animation
+    
+    :returns: SKAction
+    */
+    class func fadeNode(targetNode: SKNode, fromValue: CGFloat?, toValue: CGFloat, curveType: CurveType, mode: EasingMode, duration: NSTimeInterval) -> SKAction? {
+        
+        
+        var startValue: CGFloat
+        
+        if let fromValue = fromValue {
+            
+            startValue = fromValue
+        }
+        else {
+            
+            startValue = targetNode.alpha
+        }
+        
+        var action : SKAction?
+        
+        if let easingFunction = SKAction.getEaseFunctionForCurveType(curveType, mode: mode) {
+            
+            action = SKAction.createFloatTween(startValue, endFloat: toValue, duration: duration, easingFunction: easingFunction, setterBlock: { (node, float) -> () in
+                
+                node.alpha = float
+            })
+        }
+        
+        return action
+        
+    }
+    
+ 
     
 }
